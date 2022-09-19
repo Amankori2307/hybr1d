b3596fad84bdf374a765d514bbc3d2ee019a1f91
Aman <aman@Amans-MacBook-Pro-2.local>
2022-09-19 09:28:01 +0530

    Added basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import DetailPost from "./components/DetailPost/DetailPost";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello mate</h1>
+      <Header></Header>
+      <a href="/">Home</a>
+      <a href="/post/adfadfasdf">Detail Post</a>
+
+
+      <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/post/:postId" element={<DetailPost/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/DetailPost/DetailPost.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const DetailPost = () => {
+    return <div>
+        <h1>Detail Post</h1>
+    </div>
+}
+
+export default DetailPost
